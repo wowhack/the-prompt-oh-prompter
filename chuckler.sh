8a55dbc16e16e6e217954d451e3597465ffe78fa
David Nääs <david_naas@hotmail.com>
2014-08-05 22:16:16 +0200

    added Midiinstrument.ck to setup

--- chuckler.sh
+++ chuckler.sh
@@ -6,7 +6,7 @@ fi
 
 # setup
 if [ "$1" == "setup" ]; then 
-	chuck + fileread.ck:`pwd` Looper.ck Drum.ck LoopRecorder.ck Organ.ck
+	chuck + fileread.ck:`pwd` Looper.ck Drum.ck LoopRecorder.ck MidiInstrument.ck Organ.ck
 	exit
 fi
 
